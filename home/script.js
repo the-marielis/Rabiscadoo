b5e2fecf441f48b604a8a7aae4e4a28e5270b9dc
Marieli Teixeira <142545847+the-marielis@users.noreply.github.com>
2024-11-07 21:36:43 -0300

    alterações na tela de cadastro para inclusão de campos de mesmo tamanho

--- home/script.js
+++ home/script.js
@@ -13,3 +13,34 @@ function nextImage(){
 
 document.getElementById("radio"+count).checked = true;
 }
+
+let currentIndex = 0;
+let autoSlideInterval;
+
+function moveCarousel(direction) {
+    const images = document.querySelector('.slide');
+    const totalImages = images.children.length;
+    
+    currentIndex += direction;
+
+    if (currentIndex >= totalImages) {
+        currentIndex = 0;
+    } else if (currentIndex < 0) {
+        currentIndex = totalImages - 1;
+    }
+
+    const offset = -currentIndex * 100;
+    images.style.transform = `translateX(${offset}%)`;
+}
+
+window.onload = function() {
+    const images = document.querySelector('.slider');
+    totalImages = images.querySelectorAll('img').length;
+
+    startAutoSlide();
+
+    // Adiciona eventos para parar e reiniciar o autoSlide ao passar o mouse
+    const carousel = document.querySelector('.slider');
+    carousel.addEventListener('mouseenter', stopAutoSlide);
+    carousel.addEventListener('mouseleave', startAutoSlide);
+};
